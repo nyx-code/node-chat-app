f73ccf2ec3c9590328afafd34a890c990ce09210
nyx-coder <kadamshailesh007@gmail.com>
2018-07-15 20:20:59 +0530

    Added nice UI and modify send location button for background working

--- public/js/index.js
+++ public/js/index.js
@@ -28,11 +28,14 @@ socket.on('newLocationMessage', function(message){
 
 jQuery('#message-form').on('submit', function(e){
     e.preventDefault();
+
+    var messageTextBox = jQuery('[name=message]');
+
     socket.emit('createMessage', {
         from: 'User',
-        text: jQuery('[name=message]').val()
+        text: messageTextBox.val()
     }, function(){
-        
+        messageTextBox.val('')
     })
 });
 
@@ -43,7 +46,10 @@ locationButton.on('click',function (){
         return alert('Geolocation is not supported by your browser');
     }
 
+    locationButton.attr('disabled', 'disabled').text('Sending location...');
+
     navigator.geolocation.getCurrentPosition(function(position){
+        locationButton.removeAttr('disabled').text('Send location');
         socket.emit('createLocationMessage', {
             lat : position.coords.latitude,
             lng : position.coords.longitude
@@ -51,6 +57,7 @@ locationButton.on('click',function (){
         console.log(position);
         
     }, function() {
+        locationButton.removeAttr('disabled').text('Send location');
         alert('Unable to fetch location');
     })
 });
